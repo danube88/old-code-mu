f1b7c50f5549e3418f69625f545e4e180be7d7d5
danube88 <36491066+danube88@users.noreply.github.com>
2020-08-09 23:22:31 +0300

    Create 03.php

---
+++ task_1/03.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>На списки</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+        <h1>Списки</h1>
+
+        <h2>Список цветов</h2>
+        <ul>
+            <li>Красный</li>
+            <li>Желтый</li>
+            <li>Зеленый</li>
+            <li>Синий</li>
+        </ul>
+
+        <h2>Список студентов</h2>
+        <ol>
+            <li>Иванов</li>
+            <li>Петров</li>
+            <li>Сидоров</li>
+            <li>Николаев</li>
+        </ol>
+
+        <h2>Список студентов</h2>
+        <ol>
+            <li>Иванов</li>
+            <ul>
+                <li>Возраст - 23 года</li>
+                <li>Курс - 3</li>
+            </ul>
+            <li>Петров</li>
+            <ul>
+                <li>Возраст - 19 лет</li>
+                <li>Курс - 2</li>
+            </ul>
+            <li>Сидоров</li>
+            <ul>
+                <li>Возраст - 18 лет</li>
+                <li>Курс - 1</li>
+            </ul>
+        </ol>
+    </body>
+</html>
