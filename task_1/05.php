b70dbd1b1b2d79a8cba4b590dff1faf4daedf155
danube88 <36491066+danube88@users.noreply.github.com>
2020-08-09 23:43:11 +0300

    Create 05.php

---
+++ task_1/05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>На заголовки, абзацы и списки</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+        <a href="#">page 1</a><br/>
+        <a href="#">page 2</a><br/>
+        <a href="#">page 3</a>
+    </body>
+</html>
