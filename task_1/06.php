6cea6cebd88a5dc4a2411a49f63ca9944043e69d
danube88 <36491066+danube88@users.noreply.github.com>
2020-08-09 23:43:25 +0300

    Create 06.php

---
+++ task_1/06.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>На картинки</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+    	<img src="../img/1.jpg" width="200">
+    </body>
+</html>
