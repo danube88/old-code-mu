0fabe74007f0ea8ebf7fff0dc5eb402c84748bb4
danube88 <36491066+danube88@users.noreply.github.com>
2020-08-11 22:35:52 +0300

    create task 3

---
+++ task_3/01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>Простые таблицы</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+        <table border="1" cellspacing="0">
+            <tr>
+                <td colspan="2">1 col 1</td>
+                <td>1 col 2</td>
+            </tr>
+            <tr>
+                <td rowspan="2">2 col 1</td>
+                <td>2 col 2</td>
+                <td>2 col 3</td>
+            </tr>
+            <tr>
+                <td>3 col 1</td>
+                <td>3 col 2</td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ task_3/02.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>Простые таблицы</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+        <table border="1" cellspacing="0">
+            <tr>
+                <td colspan="3">1 col 1</td>
+            </tr>
+            <tr>
+                <td>2 col 2</td>
+                <td rowspan="2">2 col 1</td>
+                <td>2 col 3</td>
+            </tr>
+            <tr>
+                <td>3 col 1</td>
+                <td>3 col 2</td>
+            </tr>
+        </table>
+    </body>
+</html>
