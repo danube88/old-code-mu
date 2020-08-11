8b533ae854b288d60c466c4c8f4fcf32ad1b6855
danube88 <36491066+danube88@users.noreply.github.com>
2020-08-11 22:12:36 +0300

    create task 2

---
+++ task_2/01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>Простые таблицы</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+        <table border="1">
+            <tr>
+                <td>1 col 1</td>
+                <td>1 col 2</td>
+                <td>1 col 3</td>
+            </tr>
+            <tr>
+                <td>2 col 1</td>
+                <td>2 col 2</td>
+                <td>2 col 3</td>
+            </tr>
+            <tr>
+                <td>3 col 1</td>
+                <td>3 col 2</td>
+                <td>3 col 3</td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ task_2/02.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style_1.css" />
+        <title>Добавим заголовки, атрибуты width (400) и height (200)</title>
+    </head>
+    <body>
+        <div class="exit">
+            <a href="index.php">exit</a>
+        </div>
+        <table border="1" width="400px" height="200px" cellpadding="0" cellspacing="0">
+            <tr>
+                <th>1 col 1</th>
+                <th>1 col 2</th>
+                <th>1 col 3</th>
+            </tr>
+            <tr>
+                <td>2 col 1</td>
+                <td>2 col 2</td>
+                <td>2 col 3</td>
+            </tr>
+            <tr>
+                <td>3 col 1</td>
+                <td>3 col 2</td>
+                <td>3 col 3</td>
+            </tr>
+        </table>
+    </body>
+</html>
